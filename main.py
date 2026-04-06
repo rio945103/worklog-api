1bbd3e2027edfb7c573e9fdd285eda59a0954692
yokosaka <rvo9eij8@icloud.com>
2026-04-07 05:47:08 +0900

    feat: Pydanticでリクエストの型定義

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from database import init_db, insert_worklog, get_all_worklogs
 
 app = FastAPI()
-
-# 起動時にDBを初期化
 init_db()
 
-# ログ一覧を取得
+# リクエストの型定義
+class WorkLogRequest(BaseModel):
+    title: str
+    duration: int
+
 @app.get("/worklogs")
 def read_worklogs():
     return get_all_worklogs()
 
-# ログを新規作成
 @app.post("/worklogs")
-def create_worklog(title: str, duration: int):
-    insert_worklog(title, duration)
-    return {"message": "保存しました", "title": title, "duration": duration}
+def create_worklog(request: WorkLogRequest):
+    insert_worklog(request.title, request.duration)
+    return {"message": "保存しました", "title": request.title, "duration": request.duration}
